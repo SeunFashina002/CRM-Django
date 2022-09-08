715bdff194a8356c99651ddc6ed9cf6880c4db62
Seun Fashina <fashinaoluwaseun36@gmail.com>
2022-09-07 23:38:11 -0700

    added form form for creating agents

--- agents/forms.py
+++ agents/forms.py
@@ -1,8 +1,24 @@
 from django import forms
+from django.contrib.auth import get_user_model
 from leads.models import Agent
+
+User = get_user_model()
 
 class AgentCreationForm(forms.ModelForm):
     class Meta:
-        model = Agent
-        fields = ('user',)
+        model = User
+        fields = (
+            'email',
+            'username',
+            'first_name',
+            'last_name'
+            )
 
+class AssignAgentForm(forms.Form):
+    agent = forms.ModelChoiceField(queryset=Agent.objects.none())
+
+    def __init__(self, *args, **kwargs):
+        request = kwargs.pop('request')
+        agents = Agent.objects.filter(managed_by= request.user.userprofile)
+        super(AssignAgentForm, self).__init__(*args, **kwargs)
+        self.fields['agent'].queryset=agents
